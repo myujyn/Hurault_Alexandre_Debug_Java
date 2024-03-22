812c0e3638dd1a6918a0500ffb88eb0536844511
Alex <alexandre.hurault1@gmail.com>
2024-03-22 16:14:09 +0100

    Etape 4: Refactorez Analytics Counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,49 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int highbloodpressureCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	// Déclaration de variables d'instance pour les interfaces de lecture et d'écriture de symptômes.
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
 
-		int i = 0;	// set i to 0
-		int headacheCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("high blood pressure")) {
-				highbloodpressureCount++;
-			}
+	// Constructeur qui initialise la classe avec des objets implémentant ISymptomReader et ISymptomWriter.
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
+	// Utilisation de la méthode getSymptoms de l'interface ISymptomReader pour lire le fichier
+	public List<String> getSymptoms() {
 
-			line = reader.readLine();	// get another symptom
+		return getSymptoms();
+	}
+
+	// Méthode pour compter les occurences de chaque symptomes
+	public Map<String, Integer>	countSymptoms(List<String> symptoms) {
+		// Crée une nouvelle instance de HashMap, assignée à la variable symptomCounts pour stocker le compte de symptomes
+		Map<String, Integer> symptomCounts = new HashMap<>();
+		// Boucle for each qui itère sur chaque élément de la liste symptoms
+		for (String symptom : symptoms) {
+			// Cette ligne permet la mise à jour de la Hashmap avec le symptome actuel
+			// Méthode getordefault permet de compter le nombre de fois où le symptome est observé
+			// Cette valeur est mise à jour au symptome associé dans la map en utilisant la méthode put
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0)+1);
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("high blood pressure: " + highbloodpressureCount + "\n");
-		writer.close();
+		// Retourne la map avec le comptage mis à jour
+		return symptomCounts;
+	}
+	// Méthode pour trier automatiquement les symptomes par leurs noms (clé), en utilisant Treemap
+	public Map<String, Integer>	sortSymptoms(Map<String, Integer> symptoms) {
+
+		return new TreeMap<>(symptoms);
+	}
+	// Utilisation de la méthode writeSymptom de l'interface ISymptomWriter pour l'écriture des données
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+
+		writer.writeSymptoms(symptoms);
 	}
 }
